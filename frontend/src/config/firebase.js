1b2b23b472ed09fb586b853b8748e8e3a051cd5a
Asfandyar Sabri <asfandyarsabri7@gmail.com>
2023-08-25 14:30:38 +0500

    Update firebase.js

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -8,13 +8,13 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAwWzTLAxDG5himDI3AuQweaUxuMT4yqUw",
+  apiKey: "",
   authDomain: "hikal-calendar-app.firebaseapp.com",
   projectId: "hikal-calendar-app",
   storageBucket: "hikal-calendar-app.appspot.com",
-  messagingSenderId: "439673302779",
-  appId: "1:439673302779:web:6c31bb7e74f60c8f680a5e",
-  measurementId: "G-99PCL9HWWC"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
